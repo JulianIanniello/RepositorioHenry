f63c22c94063350dcc73d4878ba5535bc555edfe
JI Julian Ianniello <iannielloandres@gmail.com>
2023-10-02 02:16:39 -0300

    Actualizacion 02/10

--- JAVA.SC/HomeWorks/HomeWork M1C2/1 - JavaScript Fundamentos/Ejercicio 03.js
+++ JAVA.SC/HomeWorks/HomeWork M1C2/1 - JavaScript Fundamentos/Ejercicio 03.js
@@ -5,37 +5,60 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   return x === y
 }
+
+console.log(sonIguales(5, 5))
+console.log(sonIguales(5, 4))
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   return str1.length === str2.length
 }
+
+console.log(tienenMismaLongitud("asd", "asd"))
+console.log(tienenMismaLongitud("asd", "asds"))
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   return num < 90
 }
+console.log(menosQueNoventa(89))
+console.log(menosQueNoventa(90))
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   return num > 50
 }
+
+console.log(mayorQueCincuenta(51))
+console.log(mayorQueCincuenta(50))
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   return num % 2 === 0
 }
+
+console.log(esPar(50))
+console.log(esPar(51))
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   return num % 2 !== 0
 }
+
+console.log(mayorQueCincuenta(51))
+console.log(mayorQueCincuenta(50))
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
--- JAVA.SC/HomeWorks/HomeWork M1C2/1 - JavaScript Fundamentos/Ejercicio 04.js
+++ JAVA.SC/HomeWorks/HomeWork M1C2/1 - JavaScript Fundamentos/Ejercicio 04.js
@@ -6,32 +6,52 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   return num * num
 }
+
+console.log(elevarAlCuadrado(5))
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   return num * num * num
 }
+
+console.log(elevarAlCubo(4))
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   return Math.pow(num, exponent)
 }
+
+console.log(elevar(2,4))
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   return Math.round(num)
 }
+
+console.log(redondearNumero(2.4))
+
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   return Math.ceil(num)
 }
+
+console.log(redondearHaciaArriba(2.1))
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   return Math.floor(Math.random() * 2);
 }
+
+console.log(numeroRandom())
+console.log(numeroRandom())
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
